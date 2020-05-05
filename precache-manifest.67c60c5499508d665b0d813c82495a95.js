4cc5e5c8e06e9a61e79bf6dd22dcd0f56595e5bb
mastro-elfo <francesco.209@gmail.com>
2020-05-06 00:58:47 +0200

    Updates

---
+++ precache-manifest.67c60c5499508d665b0d813c82495a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c68dab2a6a025c15bb64e63271c74a",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "5750a09d24875544de32",
+    "url": "/todo-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bb299e468687a5799349",
+    "url": "/todo-react/static/js/2.b8f7ae5f.chunk.js"
+  },
+  {
+    "revision": "301056a7ca16ead6156b38e6917dbb28",
+    "url": "/todo-react/static/js/2.b8f7ae5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5750a09d24875544de32",
+    "url": "/todo-react/static/js/main.c70fb33a.chunk.js"
+  },
+  {
+    "revision": "9de57e018b653a710bca",
+    "url": "/todo-react/static/js/runtime-main.c56927e7.js"
+  }
+]);
